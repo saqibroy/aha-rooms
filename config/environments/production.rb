84cb635a18030af9bb0a6be6249298cab29251b7
saqibroy <saqibsohail007@gmail.com>
2016-03-22 18:54:38 +0500

    deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,8 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
+
+  config.serve_static_files = true
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
@@ -29,7 +31,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.

aeda50ab25bddfc81a06ba8ed717e1bc3c514928
saqibroy <saqibsohail007@gmail.com>
2016-03-23 18:08:45 +0500

    deploy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  def after_sign_out_path_for(resource_or_scope)
+    admin_path
+  end
   protect_from_forgery with: :exception
 end

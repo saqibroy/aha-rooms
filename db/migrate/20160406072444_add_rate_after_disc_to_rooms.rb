6f37b45ffda3c236053ae612f7c0356c6d170c3f
saqibroy <saqibsohail007@gmail.com>
2016-04-06 15:00:05 +0500

    float problem

--- db/migrate/20160406072444_add_rate_after_disc_to_rooms.rb
+++ db/migrate/20160406072444_add_rate_after_disc_to_rooms.rb
@@ -1,5 +1,5 @@
 class AddRateAfterDiscToRooms < ActiveRecord::Migration
   def change
-    add_column :rooms, :rate_after_disc, :float
+    add_column :rooms, :rate_after_disc, :float,:precision => 16, :scale => 16
   end
 end

12816d5471c173d67b7242bc465531e5039872f6
saqibroy <saqibsohail007@gmail.com>
2016-04-06 15:17:57 +0500

    fix

--- db/migrate/20160331084039_create_reservations.rb
+++ db/migrate/20160331084039_create_reservations.rb
@@ -2,7 +2,7 @@ class CreateReservations < ActiveRecord::Migration
   def change
     create_table :reservations do |t|
       t.integer :no_of_rooms
-      t.decimal :total_price, :float_value, :precision => 4, :scale => 3
+      t.integer :total_price
       t.datetime :check_in_date
       t.datetime :check_out_date
       t.integer :room_id

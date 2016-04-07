099358b41f4d22f3a4202cbee77c5c405e0ea6c8
saqibroy <saqibsohail007@gmail.com>
2016-04-07 17:47:20 +0500

    mailer done

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -5,8 +5,7 @@ class ReservationMailer < ApplicationMailer
     @admin= reservation.room.hotel.user
     @reservation= reservation
 
-    mail(to: "#{@user.email}", subject: 'Reservation Detail')
-    mail(to: "#{@admin.email}", subject: 'New Reservation Detail')
-    mail(to: "octasolutionx@gmail.com", subject: 'New Reservation Detail')
-  end
+    mail(to: @user.email, subject: 'Reservation Detail')
+    mail(to: @admin.email, subject: 'New Reservation Detail')
+    
 end

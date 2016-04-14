f724733b94966431512e1fe825d131006f2c908e
saqibroy <saqibsohail007@gmail.com>
2016-04-14 15:19:53 +0500

    front end resume

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,10 +5,22 @@ class HomeController < ApplicationController
   	@testimonialsR= Testimonial.all.order("RANDOM()").limit(2)
   end
   def reservation_new
-  	@reservation=  params.require(:reservation).permit(:no_of_rooms,:cancel, :total_price, :check_in_date, :check_out_date, :room_id, :user_id,:guests)
+  	
   	@room=Room.find(params[:id])
-  	@reservation[:total_price]= @room.rate_after_disc.to_i * (@reservation[:check_out_date].to_i - @reservation[:check_in_date].to_i)
-    @reservation[:total_price]= @reservation[:total_price].to_i * @reservation[:no_of_rooms].to_i
+  	
+    @reservation= current_user.reservations.build(reservation_params)
+    @reservation.total_price= @reservation.room.rate_after_disc.to_i * (@reservation.check_out_date.strftime("%d").to_i - @reservation.check_in_date.strftime("%d").to_i)
+    @reservation.total_price= @reservation.total_price * @reservation.no_of_rooms
+    respond_to do |format|
+      if @reservation.save
+      	@mes= "Room was Successfully reserved"
+        format.html {   }
+        format.json { render :index, status: :created, location: @reservation }
+      else
+        format.html { render :index }
+        format.json { render json: @reservation.errors, status: :unprocessable_entity }
+      end
+  end
 
   end
   def contact

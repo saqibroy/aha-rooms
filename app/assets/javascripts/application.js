78eab9cb44fe7e7013f3547e40ddf6b74d1ad270
saqibroy <saqibsohail007@gmail.com>
2016-04-16 13:32:45 +0500

    done7

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,19 +20,21 @@
 
 
 $('.translation-links a').click(function() {
-                  var lang = $(this).data('lang');
-                  var $frame = $('.goog-te-menu-frame:first');
-                  if (!$frame.size()) {
-                    alert("Error: Could not find Google translate frame.");
-                    return false;
-                  }
-                  $frame.contents().find('.goog-te-menu2-item span.text:contains('+lang+')').get(0).click();
-                  return false;
-                });
+  var lang = $(this).data('lang');
+  var $frame = $('.goog-te-menu-frame:first');
+  if (!$frame.size()) {
+    alert("Error: Could not find Google translate frame.");
+    return false;
+  }
+  $frame.contents().find('.goog-te-menu2-item span.text:contains('+lang+')').get(0).click();
+  return false;
+});
 
-
-
-
+// $(".btn-search").click(function(){
+// 	if($("checkin").val().length === 0){
+// 		$("checkin-msg").show();
+// 	}
+// });
 
 
 

5952b73077771f3182f7dafaa2d6a3d567c50a11
saqibroy <saqibsohail007@gmail.com>
2016-04-21 11:52:22 +0500

    captcha

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,10 @@
 class RegistrationsController < Devise::RegistrationsController
+	 prepend_before_action :check_captcha, only: [:create] # Change this to be any actions you want to protect.
 	
 	def create
-    super #Nothing special here.
+    super do |resource|
+      verify_recaptcha(model: resource, message: 'reCAPTCHA verification response is incorrect, please try again.')
+    end
   end
 
 
@@ -14,6 +17,14 @@ class RegistrationsController < Devise::RegistrationsController
 	def account_update_params
 		params.require(:user).permit(:name,:email,:image,:password,:password_confirmation, :current_password,:coupon)
 	end
+	def check_captcha
+      if verify_recaptcha
+        true
+      else
+        self.resource = resource_class.new sign_up_params
+        respond_with_navigational(resource) { render :new }
+      end 
+    end
 
 
 

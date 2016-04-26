5dfdd47e18ce6ec8c782a9542b589ef6748917ce
saqibroy <saqibsohail007@gmail.com>
2016-04-26 13:27:25 +0500

    smtp

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-:address => 'smtpout.asia.secureserver.net',
+:address => 'smtpout.secureserver.net',
 :domain  => 'www.aha-rooms.com',
 :port      => 25,
 :user_name => 'bookings@aha-rooms.com',

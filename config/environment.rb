6412fa8e76f39ed37110ad12becf0143d15493b1
saqibroy <saqibsohail007@gmail.com>
2016-05-10 16:25:27 +0500

    smtp gmail

--- config/environment.rb
+++ config/environment.rb
@@ -4,14 +4,5 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-  config.action_mailer.delivery_method = :smtp
-# SMTP settings for gmail
-config.action_mailer.smtp_settings = {
- :address              => "smtp.gmail.com",
- :port                 => 587,
- :user_name            => 'aharoomservice@gmail.com',
- :password             => 'booked1!',
- :authentication       => :login,
-:enable_starttls_auto => true
-}
+
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,6 +6,16 @@ Rails.application.configure do
 
 # SMTP settings for gmail
 config.action_mailer.default_url_options = { :host => 'nameless-woodland-68138.herokuapp.com' }
+config.action_mailer.delivery_method = :smtp
+# SMTP settings for gmail
+config.action_mailer.smtp_settings = {
+ :address              => "smtp.gmail.com",
+ :port                 => 587,
+ :user_name            => 'aharoomservice@gmail.com',
+ :password             => 'booked1!',
+ :authentication       => :login,
+:enable_starttls_auto => true
+}
 
 
 
